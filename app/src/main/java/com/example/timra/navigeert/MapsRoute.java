e68570160c07343f28b59132980a199ced585c97
xEeefje <_eeef@live.nl>
2017-06-01 15:53:29 +0200

    Navigatie gemaakt

--- app/src/main/java/com/example/timra/navigeert/MapsRoute.java
+++ app/src/main/java/com/example/timra/navigeert/MapsRoute.java
@@ -1,11 +1,8 @@
 package com.example.timra.navigeert;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import com.example.timra.navigeert.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,32 +10,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_route);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
-
-    public void viewCalendar(View view) {
-        Intent intent = new Intent(this, Calendar.class);
-        startActivity(intent);
-    }
-
-    public void viewToHome(View view) {
-        Intent intent = new Intent(this, MapsToHome.class);
-        startActivity(intent);
-    }
-
-    public void viewRoute(View view) {
-        Intent intent = new Intent(this, MapsRoute.class);
-        startActivity(intent);
     }
 
 
@@ -55,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng LatLng = new LatLng(52.0369370, 6.6243480);
-        mMap.addMarker(new MarkerOptions().position(LatLng).title("Je bent hier!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng));
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
